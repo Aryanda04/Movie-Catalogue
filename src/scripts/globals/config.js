c1a258adff4204f4a548c6b066f3cd6de4fb81ff
Aryanda Rizky P <aryandarizky1234@gmail.com>
2022-04-21 16:29:29 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: 'e50ea095fd5f580ef7a25ba588edd074',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
